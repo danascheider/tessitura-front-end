82761100fb740cb900a38b79bbc39fbb8ea49151
Dana Scheider <dana.scheider@gmail.com>
2014-10-05 23:05:39 -0700

    Refactor to use Backbone model's save method

--- js/canto.js
+++ js/canto.js
@@ -1,33 +1,43 @@
+// General functions
+var getAttributes = function(form) {
+  var formData = form.serializeArray();
+  var attributes = {};
+
+  for(key in formData) {
+    var chiave = formData[key]['name'];
+    if(formData[key]['value'] != '') {
+      attributes[chiave] = formData[key]['value'];
+    }
+  }
+
+  return attributes;
+}
+
+// Dashboard task display & forms
 $('a.create-task').click(function() {
   $(this).find('i.fa').toggleClass('fa-caret-right fa-caret-down');
   $(this).parent('.panel-body').find('form.task-form').slideToggle();
 })
 
 $('.task-form button:submit').click(function(e) {
+  e.preventDefault();
   var form = $(this).parent('form');
-  var formData = form.serializeArray();
-  var postData = {};
+  var attrs = getAttributes(form);
 
-  for(key in formData) {
-    var chiave = formData[key]['name'];
-    if(formData[key]['value'] != '') {
-      postData[chiave] = formData[key]['value']
-    }
+  var newTask = new Task(attrs);
+  newTask.credentials = {
+    username: 'danascheider',
+    password: 'danascheider'
   }
 
-  e.preventDefault();
-  $.ajax({
-    type: 'POST',
-    url: 'http://localhost:9292/users/1/tasks', 
-    username: 'danascheider',
-    password: 'danascheider',
-    xhrFields: {
-      withCredentials: true
-    },
-    data: JSON.stringify(postData),
-    success: function(data, status, error) {
+  newTask.save(newTask.attributes, {
+    success: function(model, response, options) {
       $('a.create-task').find('i.fa').toggleClass('fa-caret-down fa-caret-right');
       form.slideUp();
+      console.log('Success: Model saved');
+    },
+    error: function(model, xhr, options) {
+      console.log('Error: Failed to save model ' + JSON.stringify(model) + ' - xhr: ' + JSON.stringify(xhr) + ', options: ' + JSON.stringify(options));
     }
   });
 })
