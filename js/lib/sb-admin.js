f87640371fa214ff72e56e3b8837702ecaf9541d
Dana Scheider <dana.scheider@gmail.com>
2014-10-07 15:59:06 -0700

    Hide dropdown menus when clicked outside of

--- js/lib/sb-admin.js
+++ js/lib/sb-admin.js
@@ -5,19 +5,26 @@
 //Loads the correct sidebar on window load,
 //collapses the sidebar on window resize.
 $(function() {
-    $(window).bind("load resize", function() {
-        width = (this.window.innerWidth > 0) ? this.window.innerWidth : this.screen.width;
-        if (width < 768) {
-            $('div.sidebar-collapse').addClass('collapse')
-        } else {
-            $('div.sidebar-collapse').removeClass('collapse')
-        }
-    })
+  $(window).bind("load resize", function() {
+    width = (this.window.innerWidth > 0) ? this.window.innerWidth : this.screen.width;
+    if (width < 768) {
+        $('div.sidebar-collapse').addClass('collapse')
+    } else {
+        $('div.sidebar-collapse').removeClass('collapse')
+    }
+  })
+})
+
+$(function() {
+  $(document).click(function() {
+    $('.dropdown-menu').hide();
+  })
 })
 
 //Shows the things the top nav bar is supposed to show
 $(function() {
-  $('.navbar-top-links a').click(function() {
+  $('.navbar-top-links a').click(function(e) {
+    e.stopPropagation();
     var uncles = $(this).parent('li').siblings();
     $.each(uncles, function() {
       if ($(this).find('.dropdown-menu').is(':visible')) {
