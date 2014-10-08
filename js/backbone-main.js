e8864e8c4d025defd97b9cccebb1c1c708d49a3d
Dana Scheider <dana.scheider@gmail.com>
2014-10-08 11:35:46 -0700

    Begin incorporating Backbone RelationalModel

--- js/backbone-main.js
+++ js/backbone-main.js
@@ -1,5 +1,13 @@
-User = Backbone.Model.extend({
+User = Backbone.RelationalModel.extend({
   urlRoot: 'http://localhost:9292/users',
+  relations: [{
+    type:           'HasMany',
+    relatedModel:   'TaskList',
+    reverseRelation: {
+      key: 'userId'
+    }
+  }],
+  
   initialize: function() {
     // Perhaps this should trigger the API call that creates the
     // user in the database, and form submission should trigger
@@ -10,18 +18,20 @@ User = Backbone.Model.extend({
   }
 })
 
-UserCollection = Backbone.Collection.extend({
-  model: User,
-  url: 'http://localhost:9292/users'
-})
-
-Task = Backbone.Model.extend({
+Task = Backbone.RelationalModel.extend({
   urlRoot: 'http://localhost:9292/users/1/tasks',
+
+  relations: [{
+    type:         Backbone.HasOne,
+    relatedModel: 'TaskList'
+  }],
+
   initialize: function() {
     this.on('invalid', function(model, error) {
       console.log('**Validation error: ' + error + '**');
     });
   },
+
   validate: function(attrs) {
     if (!attrs.title) {
       return 'Title is required';
@@ -29,6 +39,22 @@ Task = Backbone.Model.extend({
   }
 })
 
-TaskList = Backbone.Model.extend({
-  url: '/tasks'
+TaskList = Backbone.RelationalModel.extend({
+  urlRoot: 'http://localhost:9292/user/1/tasks',
+
+  relations: [
+    {
+      type:            'HasMany',
+      relatedModel:    'Task',
+      reverseRelation: {
+        key: 'taskListId'
+      }
+    },
+
+    {
+      type:         'HasOne',
+      relatedModel: 'User',
+      key:          'userId'
+    }
+  ]
 })
