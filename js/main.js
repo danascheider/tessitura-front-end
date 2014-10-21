4dc9c4178d65de60e22ca0a32e446c53facb6c37
Dana Scheider <dana.scheider@gmail.com>
2014-10-21 11:13:38 -0700

    Tweak CSS plugin code

--- js/main.js
+++ js/main.js
@@ -1,22 +1,17 @@
 requirejs.config({
   paths: {
-    jquery     : './lib/jquery-2.1.1',
-    underscore : './lib/underscore',
-    backbone   : './lib/backbone',
-    relational : './lib/backbone-relational',
-    cookie     : './lib/cookie',
-    storage    : './lib/backbone.localStorage-min',
-    bootstrap  : './lib/bootstrap',
-    text       : './lib/text',
-    templates  : '../templates',
-    styles     : '../css'
+    jquery      : './lib/jquery-2.1.1',
+    underscore  : './lib/underscore',
+    backbone    : './lib/backbone',
+    relational  : './lib/backbone-relational',
+    cookie      : './lib/cookie',
+    storage     : './lib/backbone.localStorage-min',
+    bootstrap   : './lib/bootstrap',
+    text        : './lib/text',
+    css         : './lib/css.min'
+    templates   : '../templates',
+    stylesheets : '../css'
   },
-
-  map: {
-    '*': {
-      css: './lib/css.min'
-    }
-  }
 
   shim: {
     bootstrap  : ['jquery'],
