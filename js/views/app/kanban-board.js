afd4fd6f82aec254cb5206ebc50680712dba925c
Dana Scheider <dana.scheider@gmail.com>
2014-11-19 10:44:13 -0800

    Change flow of initialize() method so collection is fetched in initialize() instead of render

--- js/views/app/kanban-board.js
+++ js/views/app/kanban-board.js
@@ -13,47 +13,48 @@ define([
     tagName    : 'div',
     initialize : function(opts) {
       this.user = opts.user
-      this.render();
+
+      var that = this;
+      this.user.fetchIncompleteTasks().then(function(collection) {
+        that.collection = collection;
+        that.render();
+      });
     },
 
     render     : function() {
       this.$el.html(this.template());
 
       var that  = this;
-      this.user.fetchIncompleteTasks().then(function(collection) {
-        that.collection = collection;
+      this.$backlogColumn = new KanbanColumnView({
+        el         : that.$el.find('#backlog-tasks'),
+        collection : new TaskCollection(that.collection.where({backlog: true})),
+        color      : 'red',
+        icon       : 'fa-exclamation-circle',
+        headline   : 'Backlog',
+      });
 
-        that.$backlogColumn = new KanbanColumnView({
-          el         : that.$el.find('#backlog-tasks'),
-          collection : new TaskCollection(that.collection.where({backlog: true})),
-          color      : 'red',
-          icon       : 'fa-exclamation-circle',
-          headline   : 'Backlog',
-        });
+      this.$newColumn = new KanbanColumnView({
+        el         : that.$el.find('#new-tasks'),
+        collection : new TaskCollection(that.collection.where({status: 'New'})),
+        color      : 'blue',
+        icon       : 'fa-certificate',
+        headline   : 'New'
+      });
 
-        that.$newColumn = new KanbanColumnView({
-          el         : that.$el.find('#new-tasks'),
-          collection : new TaskCollection(that.collection.where({status: 'New'})),
-          color      : 'blue',
-          icon       : 'fa-certificate',
-          headline   : 'New'
-        });
+      this.$inProgressColumn = new KanbanColumnView({
+        el         : that.$el.find('#in-progress-tasks'),
+        collection : new TaskCollection(that.collection.where({status: 'In Progress'})),
+        color      : 'green',
+        icon       : 'fa-road',
+        headline   : 'In Progress'
+      });
 
-        that.$inProgressColumn = new KanbanColumnView({
-          el         : that.$el.find('#in-progress-tasks'),
-          collection : new TaskCollection(that.collection.where({status: 'In Progress'})),
-          color      : 'green',
-          icon       : 'fa-road',
-          headline   : 'In Progress'
-        });
-
-        that.$blockingColumn = new KanbanColumnView({
-          el         : that.$el.find('#blocking-tasks'),
-          collection : new TaskCollection(that.collection.where({status: 'Blocking'})),
-          color      : 'yellow',
-          icon       : 'fa-minus-circle',
-          headline   : 'Blocking'
-        });
+      this.$blockingColumn = new KanbanColumnView({
+        el         : that.$el.find('#blocking-tasks'),
+        collection : new TaskCollection(that.collection.where({status: 'Blocking'})),
+        color      : 'yellow',
+        icon       : 'fa-minus-circle',
+        headline   : 'Blocking'
       });
 
       return this;
