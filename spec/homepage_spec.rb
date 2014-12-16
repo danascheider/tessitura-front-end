be7424e35b2096f082d55a4c565af4dcabaed745
Dana Scheider <dana.scheider@gmail.com>
2014-12-16 01:47:52 -0800

    More examples covering link behavior

--- spec/homepage_spec.rb
+++ spec/homepage_spec.rb
@@ -1,21 +1,65 @@
 require 'spec_helper'
 
 describe 'Canto homepage', type: :feature do 
-  context 'no logged-in user' do 
+  before(:each) do 
+    visit '/'
+  end
+
+  it 'shows the homepage' do 
+    expect(page).to have_css('#homepage-wrapper')
+  end
+
+  it 'doesn\'t show the dashboard' do 
+    expect(page).not_to have_css('#dashboard-wrapper')
+  end
+
+  it 'doesn\'t redirect' do 
+    expect(current_path).to eql('/')
+  end
+
+  context 'link behavior' do 
+    describe 'login link' do 
+      before(:each) do 
+        visit('/')
+        within(:css, 'ul.top-nav') do 
+          find('a.login-link').click
+        end
+      end
+
+      it 'redirects to the login page' do 
+        pending('Full implementation of routing functionality')
+        expect(current_path).to eql '/login'
+      end
+
+      it 'clears the homepage view' do 
+        expect(page).not_to have_css('#homepage-wrapper')
+      end
+
+      it 'doesn\'t display the dashboard' do 
+        expect(page).not_to have_css('#dashboard-wrapper')
+      end
+
+      it 'renders the login page' do 
+        expect(page).to have_css('#login-wrapper')
+      end
+    end
+  end
+
+  context 'home route' do 
     before(:each) do 
-      visit '/'
+      visit('/#home')
     end
 
-    it 'shows the homepage' do 
+    it 'displays the homepage' do 
       expect(page).to have_css('#homepage-wrapper')
     end
 
-    it 'doesn\'t show the dashboard' do 
+    it 'doesn\'t display the dashboard' do 
       expect(page).not_to have_css('#dashboard-wrapper')
     end
 
-    it 'doesn\'t redirect' do 
-      expect(current_path).to eql('/')
+    it 'doesn\'t display the login page' do 
+      expect(page).not_to have_css('#login-wrapper')
     end
   end
 end
