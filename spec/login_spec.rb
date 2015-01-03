ec61208319839d558fe787eba95f2874b9a4de57
Dana Scheider <dana.scheider@gmail.com>
2015-01-03 15:56:47 -0800

    Eliminate :selenium tag

--- spec/login_spec.rb
+++ spec/login_spec.rb
@@ -22,15 +22,15 @@ describe 'logging in', type: :feature do
         DRIVER.get BASEPATH
         DRIVER.find_element(:css, 'ul.top-nav a.login-link').click
 
-        @form = DRIVER.find_element(:id, 'login-form')
+        form = DRIVER.find_element(:id, 'login-form')
 
-        @form.find_element(:name, 'username').send_keys 'testuser'
-        @form.find_element(:name, 'password').send_keys 'testuser'
+        form.find_element(:name, 'username').send_keys 'testuser'
+        form.find_element(:name, 'password').send_keys 'testuser'
 
-        box = @form.find_element(:name, 'remember')
+        box = form.find_element(:name, 'remember')
         box.click if box.selected?
 
-        @form.find_element(:tag_name, 'button').click
+        form.find_element(:tag_name, 'button').click
       end
 
       it 'sets \'auth\' cookie to basic auth hash' do 
@@ -43,23 +43,16 @@ describe 'logging in', type: :feature do
         expect(cookie[:value]).to eql '342'
       end
 
+      it 'sets the cookies as session cookies' do 
+        cookie = DRIVER.manage.cookie_named('auth')
+        expect(cookie[:expires]).to be_nil
+      end
+
       it 'renders the dashboard' do 
         expect(DRIVER.find_element(:id, 'dashboard-wrapper')).to be_truthy
       end
-    end
 
-    context 'with WebDriver', selenium: true do 
       it 'shows the dashboard home view' do 
-        # FIX: There should be a better way to uniquely identify the home view
-        DRIVER.get(BASEPATH)
-        DRIVER.find_element(:css, 'ul.top-nav a.login-link').click
-
-        form = DRIVER.find_element(:id, 'login-form')
-
-        form.find_element(:name, 'username').send_keys 'testuser'
-        form.find_element(:name, 'password').send_keys 'testuser'
-        form.find_element(:tag_name, 'button').click
-
         expect(DRIVER.find_element(:class_name, 'dashboard-home')).to be_displayed
       end
     end
