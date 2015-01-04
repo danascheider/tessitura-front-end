0b3a982882621713465e8dd02fd5c150e4a710cf
Dana Scheider <dana.scheider@gmail.com>
2015-01-03 16:28:28 -0800

    Tweak homepage tests to use more Selenium, less Capybara

--- spec/homepage_spec.rb
+++ spec/homepage_spec.rb
@@ -6,40 +6,34 @@ describe 'Canto homepage', type: :feature do
   end
 
   it 'shows the homepage' do 
-    expect(@homepage.wrapper).to be_true
+    expect(DRIVER.find_element(:id, 'homepage-wrapper')).to be_truthy
   end
 
   it 'doesn\'t show the login view' do 
-    expect(page).not_to have_css('#shade') # the #shade container has the login form
+    expect(DRIVER.find_element(:id, 'shade')).not_to be_displayed # the #shade container has the login form
   end
 
   it 'doesn\'t show the dashboard' do 
-    expect(page).not_to have_css('#dashboard-wrapper')
-  end
-
-  it 'doesn\'t redirect' do 
-    expect(current_path).to eql('/')
+    expect{ DRIVER.find_element(:id, 'dashboard-wrapper') }.to raise_error(Selenium::WebDriver::Error::NoSuchElementError)
   end
 
   context 'link behavior' do 
     describe 'login link' do 
       before(:each) do 
-        visit('/')
-        within(:css, 'ul.top-nav') do 
-          find('a.login-link').click
-        end
+        DRIVER.get BASEPATH
+        DRIVER.find_element(:css, 'ul.top-nav a.login-link').click
       end
 
       it 'displays the login form' do 
-        expect(page).to have_css('#login-form');
+        expect(DRIVER.find_element(:id, 'login-form')).to be_displayed
       end
 
       it 'doesn\'t display the dashboard' do 
-        expect(page).not_to have_css('#dashboard-wrapper')
+        expect{ DRIVER.find_element(:id, 'dashboard-wrapper') }.to raise_error(Selenium::WebDriver::Error::NoSuchElementError)
       end
 
       it 'doesn\'t redirect'  do 
-        expect(page).to have_css('#homepage-wrapper')
+        expect(DRIVER.find_element(:id, 'homepage-wrapper')).to be_displayed
       end
     end
   end
