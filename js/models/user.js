c2b07b0e3f9f3deb6e3feb8d190a95c3e77e2628
Dana Scheider <dana.scheider@gmail.com>
2015-01-17 16:18:12 -0800

    Add more tests and change defaultFetch method into overridden fetch and protectedFetch methods

--- js/models/user.js
+++ js/models/user.js
@@ -2,7 +2,8 @@ define([
   'backbone', 
   'api',
   'utils',
-  'collections/tasks'
+  'collections/tasks',
+  'cookie'
   ], function(Backbone, API, Utils, TaskCollection) {
 
   var User = Backbone.Model.extend({
@@ -20,11 +21,41 @@ define([
       }
     },
 
-    defaultFetch: function(successCallback) {
+    // The `fetch()` function overrides the default `fetch()` method to include
+    // the HTTP basic auth header. The `fetch()` function should only be used to
+    // retrieve user profile information for admins, because it authorizes the
+    // request using the requested user's credentials, rather than using the 
+    // credentials from the cookie. This could facilitate fetching a user's
+    // data without being logged in as that user (or without being logged in at all).
+    // 
+    // When fetching user data for a non-admin user, the `protectedFetch()` function
+    // should be called.
+
+    fetch: function(options) {
+      options = options || {};
+      options.url = (options.url || API.users.single(this.get('id')));
+
+      if (!options.beforeSend) {
+        var that = this;
+
+        options.beforeSend = function(xhr) {
+          var hash = btoa(that.get('username') + ':' + that.get('password'));
+          xhr.setRequestHeader('Authorization', 'Basic ' + hash);
+        }
+      }
+
+      return Backbone.Model.prototype.fetch.call(this, options);
+    },
+
+    // The `protectedFetch()` method fetches a user's data using the credentials
+    // stored in the cookies (which may or may not be the credentials belonging
+    // to the user whose data are being requested). This way, if a user attempts
+    // to retrieve another user's data, the response will be a 401 error.
+
+    protectedFetch: function(options) {
       this.fetch({
         url        : API.users.single(this.get('id')),
         beforeSend : Utils.authHeader,
-        success    : successCallback,
         error      : function(model, response) {
           console.log('Error: Unable to retrieve user profile: ', response);
         }
@@ -48,13 +79,13 @@ define([
       });
     },
 
-    fetchAllTasks: function() {
+    fetchTasks: function() {
       var that = this;
       var data = JSON.stringify({resource: 'Task', scope: 'incomplete'});
 
       return new Promise(function(resolve, reject) {
         that.tasks.fetch({
-          url  : API.tasks.fullCollection(that.id),
+          url  : API.tasks.fullCollection(that.get('id')),
           data : data,
           beforeSend: Utils.authHeader,
           success: function() {
--- js/spec/userSpec.js
+++ js/spec/userSpec.js
@@ -2,6 +2,7 @@ define(function(require) {
   var Backbone = require('backbone');
   var User = require('models/user');
   var API = require('api');
+  var cookie = require('cookie');
 
   if (API.base.match(/localhost/)) { throw 'Connect to test API' }
 
@@ -59,6 +60,23 @@ define(function(require) {
     describe('fetch() function', function() {
       var user = new User({id: 342, username: 'testuser', password: 'testuser', email: 'testuser@example.com'});
 
+      before(function() {
+
+        // The `fetch()` function fetches the requested user regardless of whether
+        // that user is the same as the logged-in user.
+
+        $.cookie('auth', btoa('danascheider:danascheider'));
+        $.cookie('userID', 1);
+      });
+
+      after(function() {
+
+        // Log out before moving on to other tests
+
+        $.removeCookie('auth');
+        $.removeCookie('userID');
+      });
+
       it('calls Backbone\'s fetch() function', function(done) {
 
         // Spy on the Backbone model prototype's `fetch()` method, which should
@@ -74,12 +92,86 @@ define(function(require) {
       });
 
       it('sets the auth header for the user being requested', function() {
+
+        // The `fetch()` method has to include the requested user's authorization
+        // token, regardless of what user is logged in. 
+
         var authString = 'Basic ' + btoa(user.get('username') + ':' + user.get('password'));
         var server = sinon.fakeServer.create();
 
         user.fetch();
 
         server.requests[0].requestHeaders.Authorization.should.equal(authString);
+      });
+
+      it('sends the request to the appropriate URI', function() {
+        
+        // The request should be sent to the endpoint belonging to the user
+        // whose data are being requested
+
+        var server = sinon.fakeServer.create();
+        user.fetch();
+        server.requests[0].url.should.equal(API.base + '/users/342');
+      });
+    });
+
+    describe('protectedFetch() function', function() {
+
+      var user = new User({id: 342, username: 'testuser', password: 'testuser', email: 'testuser@example.com'});
+
+      before(function() {
+
+        // The `protectedFetch()` function always uses the logged-in user's
+        // credentials in the request, regardless of which user is being 
+        // requested.
+
+        $.cookie('auth', btoa('danascheider:danascheider'));
+        $.cookie('userID', 1);
+      });
+
+      after(function() {
+
+        // Log out before moving on to other tests
+
+        $.removeCookie('auth');
+        $.removeCookie('userID');
+      });
+
+      it('calls Backbone\'s fetch() function', function(done) {
+
+        // Spy on the Backbone model prototype's `fetch()` method, which should
+        // be called from the User model's `fetch()` method (i.e., should be included
+        // in the function that overrides the model's default `fetch()` method)
+
+        sinon.spy(Backbone.Model.prototype, 'fetch');
+
+        user.protectedFetch();
+        Backbone.Model.prototype.fetch.calledOnce.should.be.true;
+        Backbone.Model.prototype.fetch.restore();
+        done();
+      });
+
+      it('sets the auth header for the logged-in user', function() {
+
+        // The `protectedFetch()` method has to includes the logged-in user's
+        // credentials, regardless of which user is being requested.
+
+        var authString = 'Basic ' + $.cookie('auth');
+        var server = sinon.fakeServer.create();
+
+        user.protectedFetch();
+
+        server.requests[0].requestHeaders.Authorization.should.equal(authString);
+      });
+
+      it('sends the request to the appropriate URI', function() {
+        
+        // The request should be sent to the endpoint belonging to the user
+        // whose data are being requested
+
+        var server = sinon.fakeServer.create();
+        user.fetch();
+        server.requests[0].url.should.equal(API.base + '/users/342');
       });
     });
   });
