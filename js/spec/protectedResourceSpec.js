59e6cb43b739e4edd1a33478a8ec739a22634cf7
Dana Scheider <dana.scheider@gmail.com>
2015-01-25 13:41:23 -0800

    Tests for the save() method

--- js/spec/protectedResourceSpec.js
+++ js/spec/protectedResourceSpec.js
@@ -6,7 +6,7 @@ define([
 ], function(Backbone, ProtectedResource, API) {
   
   describe('ProtectedResource', function() {
-    var resource;
+    var resource, server;
 
     before(function() {
       $.cookie('userID', 4);
@@ -17,6 +17,32 @@ define([
       it('returns the value of the Authorization header', function() {
         resource = new ProtectedResource();
         resource.token().should.equal('Basic ' + $.cookie('auth'));
+      });
+    });
+
+    describe('save', function() {
+      beforeEach(function() {
+        resource = new ProtectedResource();
+        resource.url = API.base + '/protected-resource';
+        server = sinon.fakeServer.create();
+      });
+
+      it('attaches an authorization header', function() {
+        resource.save();
+        server.requests[0].requestHeaders.should.haveOwnProperty('Authorization');
+      });
+
+      it('includes authorization for the logged-in user', function() {
+        resource.save();
+        var value = 'Basic ' + $.cookie('auth');
+        server.requests[0].requestHeaders.Authorization.should.equal(value);
+      });
+
+      it('calls `save` from the Backbone Model prototype', function() {
+        var stub = sinon.stub(Backbone.Model.prototype, 'save');
+        resource.save();
+        stub.calledOnce.should.be.true;
+        Backbone.Model.prototype.save.restore();
       });
     });
   });
