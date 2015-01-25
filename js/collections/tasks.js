0e1548b41cc26b131b1695e53a8d6aaeddba4b48
Dana Scheider <dana.scheider@gmail.com>
2015-01-25 11:20:36 -0800

    Remove unused methods

--- js/collections/tasks.js
+++ js/collections/tasks.js
@@ -33,38 +33,6 @@ define([
       }
 
       ProtectedCollection.prototype.fetch.call(this, opts);
-    },
-
-    repositionOnCreate : function() {
-      _.each(this.models, function(task) {
-        if (!task.isNew()) {
-          task.save({
-            url        : API.tasks.single(task.id),
-            type       : 'PUT',
-            beforeSend : Utils.authHeader,
-            error      : function() {
-              console.log('There was an error saving model ', task);
-            }
-          });
-        }
-      });
-    },
-
-    promiseCreate      : function(attributes) {
-      var that = this;
-
-      return new Promise(function(resolve, reject) {
-        that.create(attributes, {
-          url        : that.url(),
-          beforeSend : Utils.authHeader,
-          success    : function(model) {
-            resolve(model);
-          },
-          error      : function(model, response) {
-            reject(response);
-          }
-        });
-      });
     }
   });
 
