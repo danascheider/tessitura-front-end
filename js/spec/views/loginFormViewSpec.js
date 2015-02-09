bfde7fe133577e7e3b149c19d9cef634fe7495b8
Dana Scheider <dana.scheider@gmail.com>
2015-02-09 14:17:19 -0800

    Move render() to beforeEach block

--- js/spec/views/loginFormViewSpec.js
+++ js/spec/views/loginFormViewSpec.js
@@ -9,28 +9,25 @@ define(['backbone', 'views/app/login-form', 'cookie'], function(Backbone, LoginF
     describe('elements', function() {
       beforeEach(function() {
         loginForm = new LoginForm();
+        loginForm.render();
       });
 
       it('is a form', function() {
-        loginForm.render();
         loginForm.$el[0].tagName.should.equal('FORM');
         loginForm.remove();
       });
 
       it('has ID #login-form', function() {
-        loginForm.render();
         loginForm.$el[0].id.should.equal('login-form');
         loginForm.remove();
       });
 
       it('has a username field', function() {
-        loginForm.render();
         loginForm.$('input[name="username"]').length.should.equal(1);
         loginForm.remove();
       });
 
       it('has a password field', function() {
-        loginForm.render();
         loginForm.$('input[name="password"]').length.should.equal(1);
         loginForm.remove();
       });
