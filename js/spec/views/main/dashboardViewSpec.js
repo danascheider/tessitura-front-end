35f2c0c71e170c9cee42de4542ba508c961f8c65
Dana Scheider <dana.scheider@gmail.com>
2015-02-15 16:54:12 -0800

    Additional tests for main dashboard view

--- js/spec/views/main/dashboardViewSpec.js
+++ js/spec/views/main/dashboardViewSpec.js
@@ -16,10 +16,34 @@ define([
       last_name: 'User'
     });
 
+    beforeEach(function() {
+      if(typeof dashboard === 'undefined') { dashboard = new DashboardView({user: user}); }
+    });
+
     describe('constructor', function() {
       it('sets this.user', function() {
-        dashboard = new DashboardView({user: user});
-        dashboard.user.should.equal(user);
+        var newDashboard = new DashboardView({user: user});
+        newDashboard.user.should.equal(user);
+      });
+
+      it('doesn\'t call render', function() {
+        sinon.stub(Backbone.View.prototype, 'render');
+        var newDashboard = new DashboardView({user: user});
+        Backbone.View.prototype.render.called.should.be.false;
+      });
+    });
+
+    describe('elements', function() {
+      beforeEach(function() {
+        dashboard.render();
+      });
+
+      afterEach(function() {
+        dashboard.remove();
+      });
+
+      it('is a div', function() {
+        dashboard.$el[0].tagName.should.equal('DIV');
       });
     });
   });
