3d8f5666c557db2a99f6da43f5033bac7ebae769
Dana Scheider <dana.scheider@gmail.com>
2015-02-15 16:46:46 -0800

    Initial spec for main dashboard view

--- js/spec/views/main/dashboardViewSpec.js
+++ js/spec/views/main/dashboardViewSpec.js
@@ -1,3 +1,27 @@
-define(function() {
-  //
+define([
+  'jquery',
+  'underscore', 
+  'backbone', 
+  'views/app/dashboard',
+  'models/user'
+], function($, _, Backbone, DashboardView, User) {
+
+  describe('Main Dashboard View', function() {
+    var dashboard;
+    var user = new User({
+      username: 'testuser', 
+      password: 'testuser', 
+      email: 'testuser@example.com',
+      first_name: 'Test',
+      last_name: 'User'
+    });
+
+    describe('constructor', function() {
+      it('sets this.user', function() {
+        dashboard = new DashboardView({user: user});
+        dashboard.user.should.equal(user);
+      });
+    });
+  });
+
 });
