c86fa9ece27877ce06c5d0b2494c00792a696ec1
Dana Scheider <dana.scheider@gmail.com>
2015-02-20 14:04:49 -0800

    Finish tests for form's render method

--- js/spec/views/models/createFormViewSpec.js
+++ js/spec/views/models/createFormViewSpec.js
@@ -108,6 +108,13 @@ define([
         $.prototype.html.calledOnce.should.be.true;
         $.prototype.html.restore();
       });
+
+      it('calls delegateEvents', function() {
+        sinon.stub(form, 'delegateEvents');
+        form.render();
+        form.delegateEvents.calledOnce.should.be.true;
+        form.delegateEvents.restore();
+      });
     });
   });
 });
