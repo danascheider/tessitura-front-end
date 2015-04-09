2d1aa3719de63a54c6235dbd59bac84bf93d479d
Dana Scheider <dana.scheider@gmail.com>
2015-04-09 14:30:14 -0700

    Use global Canto object instead of individual requires

--- spec/support/fixtures/fixtures.js
+++ spec/support/fixtures/fixtures.js
@@ -1,15 +1,14 @@
+require(process.cwd() + '/js/canto.js');
+
 // Require the User model and create a user
-var User = require('../../../js/models/userModel.js'),
-    user = new User({id: 342, username: 'testuser', password: 'testuser', email: 'testuser@example.com', first_name: 'Test', last_name: 'User'});
+var user = new Canto.UserModel({id: 342, username: 'testuser', password: 'testuser', email: 'testuser@example.com', first_name: 'Test', last_name: 'User'});
 
 // Require the task model and create 3 tasks
-var Task  = require('../../../js/models/taskModel.js'),
-    task1 = new Task({id: 1, owner_id: 342, title: 'Task 1', status: 'New', priority: 'Low', position: 1}),
-    task2 = new Task({id: 2, owner_id: 342, title: 'Task 2', status: 'New', priority: 'Normal', position: 2}),
-    task3 = new Task({id: 3, owner_id: 342, title: 'Task 3', status: 'Complete', priority: 'Normal', position: 3});
+var task1 = new Canto.TaskModel({id: 1, owner_id: 342, title: 'Task 1', status: 'New', priority: 'Low', position: 1}),
+    task2 = new Canto.TaskModel({id: 2, owner_id: 342, title: 'Task 2', status: 'New', priority: 'Normal', position: 2}),
+    task3 = new Canto.TaskModel({id: 3, owner_id: 342, title: 'Task 3', status: 'Complete', priority: 'Normal', position: 3});
 
-var Collection = require('../../../js/collections/taskCollection.js'),
-    collection = new Collection([task1, task2, task3]);
+var collection = new Canto.TaskCollection([task1, task2, task3]);
 
 var Fixtures = {
   user            : user,
