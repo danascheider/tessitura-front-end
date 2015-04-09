4132e96dfff993dd98dd4a63b73d97a428e70b72
Dana Scheider <dana.scheider@gmail.com>
2015-04-09 14:08:15 -0700

    Refactor requires and incorporate fixtures

--- spec/collections/taskCollectionSpec.js
+++ spec/collections/taskCollectionSpec.js
@@ -1,30 +1,29 @@
 require(process.cwd() + '/spec/support/jsdom.js');
 require(process.cwd() + '/js/dependencies.js');
+require(process.cwd() + '/spec/support/env.js');
 
-var Env = require(process.cwd() + '/spec/support/env.js');
-var SUT = require(process.cwd() + '/js/collections/taskCollection.js');
-
-var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
-Backbone.$         = $;
-var TaskModel      = require(process.cwd() + '/js/models/taskModel.js'),
-    Collection     = require(process.cwd() + '/js/collections/protectedCollection.js'),
+var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest,
+    fixtures       = require(process.cwd() + '/spec/support/fixtures/fixtures.js'),
     context        = describe,
     fcontext       = fdescribe;
 
-describe('Task Collection', function() {
-  var collection;
-
-  // Instantiate the tasks that will populate the collection
-  var task1 = new TaskModel({id: 1, title: 'Task 1', status: 'New', priority: 'Normal', position: 1});
-  var task2 = new TaskModel({id: 2, title: 'Task 2', status: 'New', priority: 'Normal', position: 2});
-  var task3 = new TaskModel({id: 3, title: 'Task 3', status: 'Complete', priority: 'Normal', position: 3});
+fdescribe('Task Collection', function() {
+  beforeAll(function() {
+    _.extend(global, fixtures);
+  });
 
   beforeEach(function() {
     spyOn($, 'cookie').and.callFake(function(name) {
-      return name === 'userID' ? 342 : Env.btoa('testuser:testuser');
+      return name === 'userID' ? 342 : btoa('testuser:testuser');
     });
+  });
 
-    collection = new SUT([task1, task2, task3]);
+  afterEach(function() {
+    restoreFixtures();
+  });
+
+  afterAll(function() {
+    global = _.omit(global, fixtures);
   });
 
   describe('constructor', function() {
@@ -60,25 +59,25 @@ describe('Task Collection', function() {
   describe('core functions', function() {
     describe('fetch', function() {
       beforeEach(function() {
-        spyOn(Collection.prototype, 'fetch');
+        spyOn(Canto.ProtectedCollection.prototype, 'fetch');
       });
 
       context('normal', function() {
         it('sends the request to the collection URL #travis', function() {
           collection.fetch();
-          expect(Collection.prototype.fetch.calls.argsFor(0)[0].url).toEqual(collection.url());
+          expect(Canto.ProtectedCollection.prototype.fetch.calls.argsFor(0)[0].url).toEqual(collection.url());
         });
 
-      //   it('calls fetch on the collection prototype', function() {
-      //     collection.fetch();
-      //     expect(Collection.prototype.fetch).toHaveBeenCalled();
-      //   });
+        it('calls fetch on the collection prototype', function() {
+          collection.fetch();
+          expect(Canto.ProtectedCollection.prototype.fetch).toHaveBeenCalled();
+        });
       });
 
       context('with option `all` set to `true`', function() {
         it('sends the request to the `all` route #travis', function() {
           collection.fetch({all: true});
-          expect(Collection.prototype.fetch.calls.argsFor(0)[0].url).toEqual(collection.url() + '/all');
+          expect(Canto.ProtectedCollection.prototype.fetch.calls.argsFor(0)[0].url).toEqual(collection.url() + '/all');
         });
       });
     });
