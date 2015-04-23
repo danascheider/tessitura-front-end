eee56be238542713b3dcde6c0a634e3da9670bdc
Dana Scheider <dana.scheider@gmail.com>
2015-04-23 14:02:57 -0700

    Fix a couple errors in the step definitions

--- features/step_definitions/homepage_steps.rb
+++ features/step_definitions/homepage_steps.rb
@@ -3,10 +3,9 @@ Given(/^I am not logged in$/) do
 end
 
 Given(/^I am logged in$/) do 
-  visit('/')
+  visit('/') # It says "access denied" unless I visit some goddamned place first
   page.driver.browser.manage.add_cookie(name: 'auth', value: 'dGVzdHVzZXI6dGVzdHVzZXI=')
   page.driver.browser.manage.add_cookie(name: 'userID', value: 342)
-  expect(page.driver.browser.manage.all_cookies).to be(true)
 end
 
 When(/^I navigate to '(.*)'$/) do |url|
@@ -14,6 +13,6 @@ When(/^I navigate to '(.*)'$/) do |url|
 end
 
 Then(/^I should (not )?see the homepage$/) do |neg|
-  # if neg then expect(page).not_to have_selector('#homepage-wrapper')
-  expect(page).to have_selector('#homepage-wrapper')
+  if neg then expect(page).not_to have_selector('#homepage-wrapper')
+  else expect(page).to have_selector('#homepage-wrapper'); end
 end
