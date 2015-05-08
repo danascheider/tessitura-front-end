2cf22a7271b942fd6dd94a73d90500a4ea8ed587
Dana Scheider <dana.scheider@gmail.com>
2015-05-08 15:08:51 -0700

    Verify that users who don't accept TOU are not routed to a dashboard

--- features/register.feature
+++ features/register.feature
@@ -6,6 +6,14 @@ Feature: User registration
     And I am on the homepage
 
   Scenario: Valid registration
-    When I submit the registration form with valid data
+    When I fill out the registration form with valid data
+    And I accept the terms of use
+    And I submit the registration form
     Then I should be routed to my dashboard
     And the dashboard should have my name on it
+
+  Scenario: Terms not accepted
+    When I fill out the registration form with valid data
+    But I don't accept the terms of use
+    And I submit the registration form
+    Then I should not be routed to my dashboard
--- features/step_definitions/registration_steps.rb
+++ features/step_definitions/registration_steps.rb
@@ -2,7 +2,7 @@ Given(/^I am on the homepage$/) do
   step "I navigate to '/'"
 end
 
-When(/^I submit the registration form with valid data$/) do 
+When(/^I fill out the registration form with valid data$/) do 
   within '#registration-form' do 
     within 'fieldset.login-information' do 
       fill_in 'username', with: '22testuser22'
@@ -15,20 +15,28 @@ When(/^I submit the registration form with valid data$/) do
       fill_in 'last_name', with: 'User'
       # fill_in 'birthdate'... # Eventually birthdate should be required
     end
-
-    within 'fieldset.terms' do 
-      check 'acceptTerms'
-    end
-
-    within 'fieldset.submit' do 
-      click_button 'Create Account'
-    end
   end
 end
 
-Then(/^I should be routed to my dashboard$/) do 
+When(/^I (don't )?accept the terms of use$/) do |neg|
+  within '#registration-form fieldset.terms' do 
+    if neg then uncheck 'acceptTerms'; else check 'acceptTerms'; end
+  end
+end
+
+When(/^I submit the registration form$/) do 
+  within '#registration-form fieldset.submit' do 
+    click_button 'Create Account'
+  end
+end
+
+Then(/^I should (not )?be routed to my dashboard$/) do |neg|
   wait_for_ajax
-  expect(find('#dashboard-wrapper')).to be_visible
+  if neg
+    expect(page).not_to have_selector('#dashboard-wrapper')
+  else
+    expect(find('#dashboard-wrapper')).to be_visible
+  end
 end
 
 Then(/^the dashboard should have my name on it$/) do 
