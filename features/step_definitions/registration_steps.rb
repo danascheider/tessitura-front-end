8598b9c6d8ec0be90b63b6b3638eb8607d4ca1b0
Dana Scheider <dana.scheider@gmail.com>
2015-05-08 13:30:19 -0700

    Finish writing the 'fill out the form with valid data' step

--- features/step_definitions/registration_steps.rb
+++ features/step_definitions/registration_steps.rb
@@ -3,8 +3,25 @@ Given(/^I am on the homepage$/) do
 end
 
 When(/^I fill out the registration form with valid data$/) do 
-  within '#registration-form fieldset.login-information' do 
-    fill_in 'username', with: 'testuser22'
-    fill_in 'password', with: 'kh8980yh;lk;'
+  within '#registration-form' do 
+    within 'fieldset.login-information' do 
+      fill_in 'username', with: 'testuser22'
+      fill_in 'password', with: 'kh8980yh;lk;'
+      fill_in 'email', with: 'thisismyemail@example.com'
+    end
+
+    within 'fieldset.profile-information' do 
+      fill_in 'first_name', with: 'New'
+      fill_in 'last_name', with: 'User'
+      # fill_in 'birthdate'... # Eventually birthdate should be required
+    end
+
+    within 'fieldset.terms' do 
+      check 'acceptTerms'
+    end
+
+    within 'fieldset.submit' do 
+      click_button 'Create Account'
+    end
   end
 end
