44ea0509835217328d4ef66c084500cb7927acc4
Dana Scheider <dana.scheider@gmail.com>
2015-05-08 13:22:25 -0700

    Make tests for the registration form pass

--- features/step_definitions/registration_steps.rb
+++ features/step_definitions/registration_steps.rb
@@ -4,6 +4,7 @@ end
 
 When(/^I fill out the registration form with valid data$/) do 
   within '#registration-form fieldset.login-information' do 
-    fill_in 'username', with: 'testuser'
+    fill_in 'username', with: 'testuser22'
+    fill_in 'password', with: 'kh8980yh;lk;'
   end
 end
