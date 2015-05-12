ddbb13a731dd76d0d0d64972744fefc36c9c6998
Dana Scheider <dana.scheider@gmail.com>
2015-05-12 11:31:05 -0700

    Tests for the mark-complete checkbox

--- features/dashboard_home_elements.feature
+++ features/dashboard_home_elements.feature
@@ -30,3 +30,9 @@ Feature: Dashboard home elements
   Scenario: Expand task list item
     When I click on the '#task-1' element
     Then I should see the '#task-1' element's '.task-details' element
+
+  Scenario: Click the checkbox
+    When I click on the mark-complete checkbox inside the '#task-1' element
+    Then the mark-complete checkbox should be checked
+    And there should be a line through the title in the '#task-1' element
+    And the '#task-1' element should be removed from the DOM after a short time
--- features/step_definitions/dashboard_home_steps.rb
+++ features/step_definitions/dashboard_home_steps.rb
@@ -2,9 +2,9 @@ When(/^I navigate to the dashboard$/) do
   visit '/#dashboard'
 end
 
-Then(/^my task panel should show (\d+) tasks$/) do |num|
-  within '#task-panel' do 
-    expect(page).to have_selector('li.task-list-item', count: 2)
+When(/^I click on the mark\-complete checkbox inside the '\#task\-(\d+)' element$/) do |id|
+  within "#task-#{id}" do 
+    find('i[title="Mark complete"]').click
   end
 end
 
@@ -20,12 +20,34 @@ Then(/^I should see the '(\S*)' element's '(\S*)' element$/) do |parent, child|
   find('#task-1')
 end
 
+Then(/^my task panel should show (\d+) tasks$/) do |num|
+  within '#task-panel' do 
+    expect(page).to have_selector('li.task-list-item', count: 2)
+  end
+end
+
 Then(/^my '(.*)' widget should say I have (\d+) (.*)$/) do |name, qty, obj|
   expect(find("div[data-name=#{name}] div.huge")).to have_content qty
 end
 
+Then(/^the mark\-complete checkbox should be checked$/) do 
+  expect(page).to have_selector 'i.fa-check-square-o[title="Mark complete"]'
+end
+
+Then(/^the '\#task\-(\d+)' element should be removed from the DOM after a short time$/) do |id|
+  sleep 0.75
+  expect(page).not_to have_selector("#task-#{id}")
+end
+
 Then(/^the task details should not be visible$/) do 
-  within('#task-panel') do 
+  within '#task-panel' do 
     expect(page).not_to have_selector '.task-details'
+  end
+end
+
+Then(/^there should be a line through the title in the '\#task\-(\d+)' element$/) do |id|
+  within "#task-#{id}" do 
+    el = find('a.task-title', visible: true)
+    expect(el[:style]).to match(/text-decoration: line-through/)
   end
 end
