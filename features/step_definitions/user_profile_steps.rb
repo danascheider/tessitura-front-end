86970b0521751734b155f80eb1798934097815f8
Dana Scheider <dana.scheider@gmail.com>
2015-06-14 13:52:26 -0700

    Make initial specs pass for the appearance of inputs when user double-clicks profile attribute

--- features/step_definitions/user_profile_steps.rb
+++ features/step_definitions/user_profile_steps.rb
@@ -1,3 +1,16 @@
+When(/^I double-click on the (.*) field$/) do |element|
+  within "#profile-tab tr[data-attribute=#{element}]" do 
+    find('td').double_click
+  end
+end
+
+Then(/^an input should appear in the (.*) field$/) do |element|
+  within "#profile-tab tr[data-attribute=#{element}]" do 
+    @input = find('input')
+    expect(@input).to be_truthy
+  end
+end
+
 Then(/^I should see my '(.*)' attribute$/) do |attribute|
   within '#profile-info' do
     expect(page).to have_text "#{attribute.match(/email/i) ? 'E-mail' : attribute.capitalize}:"
@@ -7,4 +20,10 @@ end
 Then(/^I should see that my (.*) is (.*)$/) do |attribute, value|
   attribute = attribute.match(/email/i) ? 'E-mail' : attribute.capitalize
   expect(page).to have_text "#{attribute}: #{value}"
+end
+
+Then(/^the input should contain the text (.*)$/) do |text|
+  within '#profile-info' do 
+    expect(@input.value).to eq text
+  end
 end
--- features/user_profile.feature
+++ features/user_profile.feature
@@ -1,4 +1,18 @@
 @javascript
 Feature: User profile
-  #
-  #
+
+  Background:
+    Given I am on the '/#profile' page
+
+  Scenario Outline: Edit profile
+    When I double-click on the <element> field
+    Then an input should appear in the <element> field
+    And the input should contain the text <value>
+
+    Examples:
+      | element   | value                |
+      | username  | testuser             |
+      | email     | testuser@example.com |
+      | birthdate |                      |
+      | fach      |                      |
+      | city      |                      |
