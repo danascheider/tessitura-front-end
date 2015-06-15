1fda7c6973066792a7b9c2815b1b8376cb8d5521
Dana Scheider <dana.scheider@gmail.com>
2015-06-15 15:55:28 -0700

    Passing features

--- features/step_definitions/user_profile_steps.rb
+++ features/step_definitions/user_profile_steps.rb
@@ -1,4 +1,14 @@
-When(/^I double-click on the (.*) field$/) do |element|
+Given(/^the input inside the '(.*)' field is visible$/) do |field|
+  step "I double-click on the '#{field}' field"
+end
+
+When(/^I submit the '(.*)' form with value '(.*)'$/) do |name, value|
+  step "I double-click on the '#city' element"
+  fill_in name, with: value
+  find("input[name=#{name}]").native.send_key(:Enter)
+end
+
+When(/^I double-click on the '(.*)' field$/) do |element|
   within "#profile-tab span.profile-field##{element}" do 
     find('span.p').double_click
   end
@@ -14,14 +24,20 @@ Then(/^an input called (.*) should appear$/) do |name|
 end
 
 Then(/^an input should appear in the (.*) field$/) do |element|
-  within "#profile-tab tr[data-attribute=#{element}]" do 
-    @input = find('input')
+  within "tr[data-attribute=#{element}] .profile-field" do 
+    @input = find('span.input > input')
     expect(@input).to be_truthy
   end
 end
 
 Then(/^I should not see an input called '(.*)'$/) do |name|
   expect(page).to have_no_selector "input[name=#{name}]"
+end
+
+Then(/^I should not see the input inside the '(.*)' element$/) do |el|
+  within el do 
+    expect(page).to have_no_selector 'input'
+  end
 end
 
 Then(/^I should not see the 'span\.p' element in the (.*) field$/) do |element|
@@ -44,5 +60,11 @@ end
 Then(/^the input should contain the text (?:'?)([^']*)(?:'?)$/) do |text|
   within '#profile-info' do 
     expect(@input.value).to eq text
+  end
+end
+
+Then(/^the text in the '(.*)' element should be '(.*)'$/) do |el, text|
+  within el do 
+    expect(page).to have_content text
   end
 end
