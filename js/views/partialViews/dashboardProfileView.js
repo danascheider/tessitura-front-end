f5dbe16c8c5b889dd3f5ab613056770096af042c
Dana Scheider <dana.scheider@gmail.com>
2015-06-21 16:01:22 -0700

    Add dashboard profile view

---
+++ js/views/partialViews/dashboardProfileView.js
@@ -0,0 +1,56 @@
+Tessitura.DashboardProfileView = Tessitura.View.extend({
+  className   : 'user-profile',
+  id          : 'page-wrapper',
+  template    : JST['partials/profile'],
+
+  events      : {
+    'click' : 'hideInputs'
+  },
+
+  /* Tessitura View Properties
+  /**************************************************************************************/
+
+  klass       : 'DashboardProfileView',
+  family      : 'Tessitura.View',
+  superFamily : 'Backbone.View',
+  types       : function() {
+    return Tessitura.View.prototype.types().concat(this.klass, 'DashboardView', 'DashboardProfileView');
+  },
+
+  /* Event Callbacks
+  /**************************************************************************************/
+
+  hideInputs: function(e) {
+    if(!$(e.target).is('input')) { this.profileView.hideInputs(); }
+  },
+
+  /* Special Functions
+  /**************************************************************************************/
+
+  setUser   : function(user) {
+    this.model = user;
+    this.profileView = new Tessitura.UserProfileView({model: user});
+    this.childViews = [this.profileView];
+    return this;
+  },
+
+  /* Core View Functions
+  /**************************************************************************************/
+
+  initialize  : function(args) {
+    args = args || {};
+    if(args.model) { this.setUser(args.model); }
+    _.bindAll(this, 'hideInputs');
+  },
+
+  render      : function() {
+    var that = this;
+
+    return Tessitura.View.prototype.render.call(this, this.template(), function() {
+      that.profileView.render();
+      that.$('.col-md-8').html(that.profileView.$el);
+    });
+  }
+});
+
+module.exports = Tessitura.DashboardProfileView;
