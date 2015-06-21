45e68a901988637b202e8397a09bcc6fff0f232a
Dana Scheider <dana.scheider@gmail.com>
2015-06-21 15:59:53 -0700

    Remove annoying comment and change naming convention

--- js/views/partialViews/dashboardTaskView.js
+++ js/views/partialViews/dashboardTaskView.js
@@ -1,88 +1,4 @@
-/***************************************************************************
- *                                                                         *
- * DASHBOARD TASK VIEW                                                     *
- *                                                                         *
- * The dashboard task view is the page that shows detailed information     *
- * about the user's tasks. Currently, it takes the form of a Kanban        *
- * board, but I'm considering other possibilities as well.                 *
- *                                                                         *
- * CONTENTS                                                          LINE  *
- * Core Requires .................................................... 84   *
- * Module ........................................................... 91   *
- *   Backbone View Properties ....................................... 96   *
- *     template ..................................................... 96   *
- *     id ........................................................... 97   *
- *   Tessitura View Properties ...................................... --   *
- *     klass ........................................................ --   *
- *     family ....................................................... --   *
- *     superFamily .................................................. --   *
- *     types ........................................................ --   *
- *   View Events .................................................... --   *
- *     click $el .................................................... --   *
- *     click li.dropdown ............................................ --   *
- *   Event Callbacks ................................................ --   *
- *     hideDropdownMenus() .......................................... --   *
- *       when no menus are open .................................... ---   *
- *       when a menu is visible .................................... ---   *
- *       when the clicked-on object is inside the menu ............. ---   *
- *     toggleDropdownMenu() ........................................ ---   *
- *       when none of the menus is open ............................ ---   *
- *         adds the .open class to the target menu ................. ---   *
- *         doesn't add the .open class to the other menus .......... ---   *
- *       when another menu is open ................................. ---   *
- *         removes the .open class from the open menu .............. ---   *
- *         adds the .open class to the target menu ................. ---   *
- *       when the target menu is open .............................. ---   *
- *         removes the .open class from the target menu ............ ---   *
- *         doesn't open any other menus ............................ ---   *
- *     showHomeView() .............................................. ---   *
- *       when the main dash and home view are visible .............. ---   *
- *         doesn't re-render the main dash ......................... ---   *
- *         renders the home view ................................... ---   *
- *         attaches the home view to the DOM ....................... ---   *
- *       when the main dash and task view are visible .............. ---   *
- *         doesn't re-render the main dash ......................... ---   *
- *         removes the task view ................................... ---   *
- *         renders the home view ................................... ---   *
- *         attaches the home view to the DOM ....................... ---   *
- *       when the main dash isn't visible .......................... ---   *
- *         renders the main dash view .............................. ---   *
- *         renders the home view ................................... ---   *
- *         attaches the home view to the DOM ....................... ---   *
- *     showTaskView() .............................................. ---   *
- *       when the main dash and home view are visible .............. ---   *
- *         doesn't re-render the main dash ......................... ---   *
- *         removes the home view ................................... ---   *
- *         renders the task view ................................... ---   *
- *         attaches the task view to the DOM ....................... ---   *
- *       when the main dash and task view are visible .............. ---   *
- *         doesn't re-render the main dash ......................... ---   *
- *         renders the task view ................................... ---   *
- *         attaches the task view to the DOM ....................... ---   *
- *       when the main dash isn't visible .......................... ---   *
- *         doesn't re-render the main dash ......................... ---   *
- *         renders the task view ................................... ---   *
- *         attaches the task view to the DOM ....................... ---   *
- *   Special Functions .............................................. 97   *
- *     initialize() ................................................. --   *
- *     remove() .................................................... 105   *
- *     render() ..................................................... 97   *
- *   Special Functions .............................................. 69   *
- *     isA() ....................................................... ---   *
- *       returns true with argument DashboardTaskView .............. ---   *
- *       returns false with another argument ....................... ---   *
- *     setUser() .................................................... 69   *
- *       sets this.user ............................................ ---   *
- *       calls setUser on the home view ............................ ---   *
- *       calls setUser on the task view ............................ ---   *
- *                                                                         *
-/***************************************************************************/
-
-/****************************************************************************
- * BEGIN MODULE                                                             *
-/****************************************************************************/
-
-var DashboardTaskView = Tessitura.View.extend({
+Tessitura.DashboardTaskView = Tessitura.View.extend({
 
   /* Backbone View Properties
   /**************************************************************************/
@@ -223,4 +139,4 @@ var DashboardTaskView = Tessitura.View.extend({
   }
 });
 
-module.exports = DashboardTaskView
+module.exports = Tessitura.DashboardTaskView
