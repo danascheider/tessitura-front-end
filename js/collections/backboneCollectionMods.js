49899e76bdb25f85146c6b7439f8b78153af5582
Dana Scheider <dana.scheider@gmail.com>
2015-06-21 15:31:58 -0700

    Add core Tessitura collection properties to Backbone collection prototype

---
+++ js/collections/backboneCollectionMods.js
@@ -0,0 +1,11 @@
+require('backbone');
+require('underscore');
+
+Backbone.Collection.prototype.klass = 'Backbone.Collection';
+Backbone.Collection.prototype.types = function() {
+  return ['Backbone.Collection']
+};
+
+Backbone.Collection.prototype.isA = function(type) {
+  return this.types().indexOf(type) > -1 ? true : false;
+};
