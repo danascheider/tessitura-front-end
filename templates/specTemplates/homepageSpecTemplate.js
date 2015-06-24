7f2e9146bf5dbe668a2e2df9b67ef4a89786e7c0
Dana Scheider <dana.scheider@gmail.com>
2015-06-23 22:33:46 -0700

    Delete spec views since I got rid of the Jasmine UI tests

--- templates/specTemplates/homepageSpecTemplate.js
+++
@@ -1,3 +0,0 @@
-var string = "<div id='triggers'>\n  <ul>\n    <li><a href='#' data-method='hideLoginForm'>hideLoginForm</a></li>\n    <li><a href='#' data-method='showLoginForm'>showLoginForm</a></li>\n    <li><a href='#' data-method='toggleLoginForm'>toggleLoginForm</a></li>\n  </ul>\n</div>\n<div id='view'></div>";
-
-module.exports = string;
