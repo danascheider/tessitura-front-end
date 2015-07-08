f94780b9c99a295fc37b8a103f18006c0021ffdd
Dana Scheider <dana.scheider@gmail.com>
2015-07-08 11:17:28 -0700

    Handle showEditForm event on children of the dashboard task view

--- spec/views/partialViews/dashboardTaskViewSpec.js
+++ spec/views/partialViews/dashboardTaskViewSpec.js
@@ -91,30 +91,32 @@ describe('Dashboard Task View', function() {
   describe('event wiring', function() {
     describe('add task to collection', function() {
       it('calls allocate() #dashboardTaskView #partialView #view #travis', function() {
-        pending('FUFNR');
         spyOn(Tessitura.DashboardTaskView.prototype, 'allocate');
         newView = new Tessitura.DashboardTaskView({user: user});
         newView.render();
         newTask = new Tessitura.TaskModel({title: 'Foobar'});
-        newView.user.tasks.add(newTask);
+        newView.collection.add(newTask);
         expect(Tessitura.DashboardTaskView.prototype.allocate).toHaveBeenCalled();
       });
     });
 
     describe('change task status', function() {
       it('calls changeStatus() #dashboardTaskView #partialView #view #travis', function() {
-        pending('FUFNR');
         spyOn(Tessitura.DashboardTaskView.prototype, 'changeStatus');
         newView = new Tessitura.DashboardTaskView({user: user});
         newView.render();
-        task1.set({status: 'In Progress'});
+        newView.collection.trigger('change:status', task1);
         expect(Tessitura.DashboardTaskView.prototype.changeStatus).toHaveBeenCalled();
       });
     });
 
-    describe('remove task from collection', function() {
-      it('calls removeAndRender() #dashboardTaskView #partialView #view #travis', function() {
-        pending('FUFNR');
+    describe('showEditForm on kanban column', function() {
+      it('calls showEditForm() #dashboardTaskView #partialView #view #travis', function() {
+        spyOn(Tessitura.DashboardTaskView.prototype, 'showEditForm');
+        newView = new Tessitura.DashboardTaskView({user: user, collection: collection});
+        newView.render();
+        newView.newColumnView.showEditForm(task1);
+        expect(Tessitura.DashboardTaskView.prototype.showEditForm).toHaveBeenCalled();
       });
     });
   });
@@ -193,6 +195,15 @@ describe('Dashboard Task View', function() {
           expect(view.findNewView(task1)).toBe(view.newColumnView);
         });
       })
+    });
+
+    describe('showEditForm()', function() {
+      it('triggers the showEditForm event #dashboardTaskView #partialView #view #travis', function() {
+        var spy = jasmine.createSpy();
+        view.on('showEditForm', spy);
+        view.showEditForm(task1);
+        expect(spy).toHaveBeenCalledWith(task1);
+      });
     });
   });
 
