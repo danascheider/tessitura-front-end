90b0ce97f9ab8e859a07e47344890667af34dd46
Dana Scheider <dana.scheider@gmail.com>
2015-07-29 14:19:22 -0700

    Add comments

--- js/models/fachModel.js
+++ js/models/fachModel.js
@@ -11,9 +11,18 @@ Tessitura.FachModel = Tessitura.Model.extend({
   },
 
   displayTitle : function() {
+
+    // The basic voice type - soprano, mezzo-soprano, contralto, countertenor,
+    // tenor, baritone, or bass - capitalized
+
     var str = this.get('type').charAt(0).toUpperCase() + this.get('type').slice(1);
+
+    // If the fach has a quality - i.e., lyric or dramatic - or the 'coloratura' 
+    // attribute is set to `true`, these data are added to the string
+    
     if(this.get('quality')) { str = str.concat(', ' + this.get('quality')); }
     if(this.get('coloratura') === true) { str = str.concat(', coloratura'); }
+
     return str;
   }
 });
