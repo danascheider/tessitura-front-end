d83fe0b55ae1603a4d1d54617ed34adfe1a68ccc
Dana Scheider <dana.scheider@gmail.com>
2015-08-24 10:46:18 -0700

    Update fixtures, fix error where task4 and task5 objects are not reset, and add organizations and organization collection

--- spec/support/fixtures/fixtures.js
+++ spec/support/fixtures/fixtures.js
@@ -5,22 +5,26 @@ require('../../../js/tessitura.js');
 var Fixtures = {};
 
 // Require the User model and create a user
-/* istanbul ignore next */
 var user = new Tessitura.UserModel({id: 1, username: 'testuser', password: 'testuser', email: 'testuser@example.com', first_name: 'Test', last_name: 'User'});
 
 // Require the task model and create 3 tasks
-/* istanbul ignore next */
+
 var task1 = new Tessitura.TaskModel({id: 1, owner_id: 1, title: 'Task 1', status: 'New', priority: 'Low', position: 1}),
     task2 = new Tessitura.TaskModel({id: 2, owner_id: 1, title: 'Task 2', status: 'New', priority: 'Normal', position: 2}),
     task3 = new Tessitura.TaskModel({id: 3, owner_id: 1, title: 'Task 3', status: 'Blocking', priority: 'High', position: 3}),
     task4 = new Tessitura.TaskModel({id: 4, owner_id: 1, title: 'Task 4', status: 'In Progress', priority: 'High', backlog: true, position: 4}),
     task5 = new Tessitura.TaskModel({id: 5, owner_id: 1, title: 'Task 5', status: 'Complete', priority: 'Normal', position: 5});
-/* istanbul ignore next */
+
 var collection = user.tasks = new Tessitura.TaskCollection([task1, task2, task3, task4, task5]);
 
 var fach  = new Tessitura.FachModel({id: 1, type: 'soprano', quality: 'lyric', coloratura: true});
 
-/* istanbul ignore next */
+var org1 = new Tessitura.OrganizationModel({id: 1, name: 'Organization 1'}),
+    org2 = new Tessitura.OrganizationModel({id: 2, name: 'Organization 2'}),
+    org3 = new Tessitura.OrganizationModel({id: 3, name: 'Organization 3'});
+
+var organizations = new Tessitura.OrganizationCollection([org1, org2, org3]);
+
 Fixtures = {
   user            : user,
   task1           : task1,
@@ -38,10 +42,16 @@ Fixtures = {
     this.task1.set({id: 1, owner_id: 1, title: 'Task 1', status: 'New', priority: 'Low', position: 1}, {silent: true});
     this.task2.set({id: 2, owner_id: 1, title: 'Task 2', status: 'New', priority: 'Normal', position: 2}, {silent: true});
     this.task3.set({id: 3, owner_id: 1, title: 'Task 3', status: 'Complete', priority: 'Normal', position: 3}, {silent: true});
-    this.collection.reset([task1, task2, task3]);
-    this.fach.set({id: 1, type: 'soprano', quality: 'lyric', coloratura: true});
+    this.task4.set({id: 4, owner_id: 1, title: 'Task 4', status: 'In Progress', priority: 'High', backlog: true, position: 4}, {silent: true});
+    this.task5.set({id: 5, owner_id: 1, title: 'Task 5', status: 'Complete', priority: 'Normal', position: 5}, {silent: true});
+    this.collection.reset([task1, task2, task3], {silent: true});
+    this.fach.set({id: 1, type: 'soprano', quality: 'lyric', coloratura: true}, {silent: true});
+
+    this.org1.set({id: 1, name: 'Organization 1'}, {silent: true});
+    this.org2.set({id: 2, name: 'Organization 2'}, {silent: true});
+    this.org3.set({id: 3, name: 'Organization 3'}, {silent: true});
+    this.organizations.reset([org1, org2, org3], {silent: true});
   }
 }
 
-/* istanbul ignore next */
 module.exports = Fixtures;
