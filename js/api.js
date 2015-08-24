bfdc8b69d5420ddfc015c6653936c3a7b2dc7b6b
Dana Scheider <dana.scheider@gmail.com>
2015-08-24 14:29:55 -0700

    Add routes for organizations

--- js/api.js
+++ js/api.js
@@ -1,26 +1,30 @@
-/* istanbul ignore next */ var test    = require('./apiOptions.js').useTestAPI;
-/* istanbul ignore next */ var BaseURL = test ? 'http://api.canto-test.com:3000' : 'https://api.tessitura.io';
+var test    = require('./apiOptions.js').useTestAPI;
+var BaseURL = test ? 'http://api.canto-test.com:3000' : 'https://api.tessitura.io';
 
-/* istanbul ignore next */
 var API = {
-  base  : BaseURL,
-  login : BaseURL + '/login',
-  users : {
+  base          : BaseURL,
+  login         : BaseURL + '/login',
+  users         : {
     root           : BaseURL + '/users',
     collection     : BaseURL + '/users',
     single         : function(uid) { return BaseURL + '/users/' + uid; },
   },
-  tasks : {
+  tasks         : {
     root           : BaseURL + '/tasks',
     collection     : function(uid) { return BaseURL + '/users/' + uid + '/tasks'; },
     fullCollection : function(uid) { return BaseURL + '/users/' + uid + '/tasks/all'; },
     single         : function(taskID) { return BaseURL + '/tasks/' + taskID; }
   },
-  fachs : {
+  fachs         : {
     root           : BaseURL + '/fachs',
     collection     : BaseURL + '/fachs',
     single         : function(fachID) { return BaseURL + '/fachs/' + fachID; }
+  },
+  organizations : {
+    root           : BaseURL + '/organizations',
+    collection     : BaseURL + '/organizations',
+    single         : function(organizationID) { return BaseURL + '/organizations/' + organizationID; }
   }
 };
 
-/* istanbul ignore next */ module.exports = API;
+module.exports = API;
