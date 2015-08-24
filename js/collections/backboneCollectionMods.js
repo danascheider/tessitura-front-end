0a1b3a7be0da5f95483e6123e26c6c4223ace04b
Dana Scheider <dana.scheider@gmail.com>
2015-08-24 11:35:04 -0700

    Delete Istanbul comments from the backboneCollectionMods.js file

--- js/collections/backboneCollectionMods.js
+++ js/collections/backboneCollectionMods.js
@@ -1,14 +1,12 @@
-/* istanbul ignore next */ require('backbone');
-/* istanbul ignore next */ require('underscore');
+require('backbone');
+require('underscore');
 
-/* istanbul ignore next */ Backbone.Collection.prototype.klass = 'Backbone.Collection';
+Backbone.Collection.prototype.klass = 'Backbone.Collection';
 
-/* istanbul ignore next */
 Backbone.Collection.prototype.types = function() {
   return ['Backbone.Collection']
 };
 
-/* istanbul ignore next */
 Backbone.Collection.prototype.isA = function(type) {
   return this.types().indexOf(type) > -1 ? true : false;
 };
