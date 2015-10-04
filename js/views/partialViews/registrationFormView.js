ed734c39c0521db5672b1b15fe9e1241838e0c8c
Dana Scheider <dana.scheider@gmail.com>
2015-10-04 13:40:10 -0700

    Actually fail validations when the required fields aren't filled out... that'll help

--- js/views/partialViews/registrationFormView.js
+++ js/views/partialViews/registrationFormView.js
@@ -67,30 +67,37 @@ Tessitura.RegistrationFormView = Tessitura.View.extend({
 
     if(!data.username) {
       this.$('input[name=username]').addClass('has-error');
+      return false;
     }
 
     if(!data.password) {
       this.$('input[name=password]').addClass('has-error');
+      return false;
     }
 
     if(!data.passwordConfirmation) {
       this.$('input[name=passwordConfirmation]').addClass('has-error');
+      return false;
     }
 
     if(!data.email) {
       this.$('input[name=email]').addClass('has-error');
+      return false;
     }
 
     if(!data.emailConfirmation) {
       this.$('input[name=emailConfirmation]').addClass('has-error');
+      return false;
     }
 
     if(!data.first_name) {
       this.$('input[name=first_name]').addClass('has-error');
+      return false;
     }
 
     if(!data.last_name) {
       this.$('input[name=last_name]').addClass('has-error');
+      return false;
     }
 
     delete data.acceptTerms;
